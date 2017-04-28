565df7fab12f08886f3ba54a9300500f6ff02a6b
ankerm <anker@mollers.nu>
2017-04-28 14:33:25 +0200

    Opgave 04

---
+++ Opg04Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg04Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string forNavn = "Anker";
+            string efterNavn = "Møller";
+            string fuldNavn = forNavn + " " + efterNavn;
+            Console.WriteLine(fuldNavn);
+            string stortNavn = fuldNavn.ToUpper();
+            Console.WriteLine(stortNavn);
+            string lilleNavn = fuldNavn.ToLower();
+            Console.WriteLine(lilleNavn);
+            string partNavn = fuldNavn.Substring(6, 6);
+            Console.WriteLine(partNavn);
+            string[] navne = fuldNavn.Split(' ');
+            foreach (var item in navne)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
