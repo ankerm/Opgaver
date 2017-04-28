a0625fede466eab4744bdc2515d48f427ac9211b
ankerm <anker@mollers.nu>
2017-04-28 08:46:51 +0200

    Start

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg01HelloWorld
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string navn;
+            string navnMedStort;
+
+            Console.WriteLine("Indtast navn");
+            navn = Console.ReadLine();
+            navnMedStort = navn.ToUpper();
+            Console.WriteLine("Velkommen " + navnMedStort);
+            Console.WriteLine("Tryk en tast");
+            Console.ReadKey();
+        }
+    }
+}
