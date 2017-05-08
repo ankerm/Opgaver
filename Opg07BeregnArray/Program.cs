c71cddb8dd2d56a740221495d27b0be5ceeebae3
ankerm <anker@mollers.nu>
2017-05-08 11:04:04 +0200

    Opg07

---
+++ Opg07BeregnArray/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg07BeregnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 10, 7, 6, 3, 1, 50, 3 };
+            var res = BeregnOgSorterArray(test);
+            Console.WriteLine(res.sum);
+            Console.WriteLine(res.gennemsnit);
+            foreach (var item in test)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        static ArrayResultat BeregnOgSorterArray(int[] a)
+        {
+            ArrayResultat r;
+
+            r.sum = a.Sum();
+            r.gennemsnit = a.Average();
+            Array.Sort(a);
+            return r;
+        }
+
+        struct ArrayResultat
+        {
+            public double sum;
+            public double gennemsnit;
+
+        }
+    }
+}
