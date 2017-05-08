4145f266699008127c3e4e1597062d526b1582f6
ankerm <anker@mollers.nu>
2017-05-08 13:06:08 +0200

    Nye opgaver

---
+++ Opg07Overloading/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg07Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Beregn(1,2));
+            Console.WriteLine(Beregn(1, 2, 3));
+            Console.WriteLine(Beregn(1, 2, 4, 5));
+
+        }
+
+        static int Beregn(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Beregn(int a, int b, int c)
+        {
+            return Beregn(a, b) + c;
+        }
+
+        static int Beregn(int a, int b, int c, int d)
+        {
+            return Beregn(a, b, c) + d;
+        }
+    }
+}
---
+++ Opg09Rekursion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg09Rekursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SkrivTal(1);
+        }
+        static void SkrivTal(int i)
+        {
+            Console.WriteLine(i);
+            if (i < 10)
+            {
+                SkrivTal(i + 1);
+            }
+        }
+    }
+}
