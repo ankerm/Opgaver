6d6df183092953dcc9417894612fe2bbce84e9e8
ankerm <anker@mollers.nu>
2017-05-09 11:19:08 +0200

    Opgave 12

---
+++ Opg12Properties2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg12Properties2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            p1.Fornavn = "Anker";
+            p1.Efternavn = "Møl";
+            Console.WriteLine(p1.FuldtNavn());
+            
+        }
+    }
+
+    class Person
+    {
+        public string Fornavn { get; set; }
+        private string efternavn;
+
+        public string Efternavn
+        {
+            get { return efternavn; }
+            set
+            {
+                if (value.Length > 3)
+                    efternavn = value;
+                else
+                    efternavn = "";
+
+                 }
+        }
+        public string FuldtNavn ()
+        {
+            return Fornavn + " " + efternavn;
+        }
+
+    }
+}
