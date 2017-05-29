e23324b2d7b0c23867f56d4d53cbdb423a230d23
admin <admin@AR-113-07>
2017-05-29 15:50:24 +0200

    Opg19

---
+++ Opg19Compare/Hund.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg19Compare
+{
+    class Hund : IComparable
+    {
+        public string Navn { get; set; }
+        public int Alder { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Hund a = obj as Hund;
+            if (this.Alder > a.Alder)
+                return 1;
+            else if (this.Alder < a.Alder)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ Opg19Compare/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg19Compare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hund h1 = new Hund();
+            h1.Alder = 5;
+            h1.Navn = "Plet";
+            Hund h2 = new Hund();
+            h2.Alder = 7;
+            h2.Navn = "Fido";
+            Hund h3 = new Hund();
+            h3.Alder = 2;
+            h3.Navn = "Pluto";
+            Hund[] a = new Hund[3];
+            a[0] = h1;
+            a[1] = h2;
+            a[2] = h3;
+
+            Hund[] a2 = new Hund[2] { new Hund() { Alder = 10, Navn = "Bulder" }, new Hund() { Alder = 5, Navn = "Frodo" } };
+
+            foreach (var h in a2)
+            {
+                Console.WriteLine("{0} {1} år", h.Navn, h.Alder);
+            }
+            Array.Sort(a2);
+            Console.WriteLine("Sorteret efter alder");
+            foreach (var h in a2)
+            {
+                Console.WriteLine("{0} {1} år", h.Navn, h.Alder);
+            }
+
+        }
+    }
+}
