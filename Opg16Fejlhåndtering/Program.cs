f2eccce10615e1e29f097a6f8b713c9a2637d2e9
admin <admin@AR-113-07>
2017-05-29 11:20:45 +0200

    Opg16

---
+++ Opg16Fejlhåndtering/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg16Fejlhåndtering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+
+        }
+        
+        private static void Run()
+        {
+            try {
+                Console.WriteLine("Indtast 1. tal");
+                int tal1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Indtast 2. tal");
+                int tal2 = Convert.ToInt32(Console.ReadLine());
+                int res = tal1 + tal2;
+                Console.WriteLine("Resultatet er {0}", res);
+
+            }
+            catch(System.FormatException e)
+            {
+                Console.WriteLine("Der er sket en fejl: {0}", e.Message);
+            }
+        }
+    }
+}
