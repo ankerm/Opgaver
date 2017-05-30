ff2d7219d5178594a0ce772a9f8ea757a769c1df
admin <admin@AR-113-07>
2017-05-30 13:00:41 +0200

    Opg23

---
+++ Opg23Delegates/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg23Delegates
+{
+    public delegate int BeregnDelegate(int a, int b);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Beregner(2, 3, Plus));
+            Console.WriteLine(Beregner(2, 3, Minus));
+            Console.WriteLine(Beregner(10, 5, Divider));
+            Console.WriteLine(Beregner(2, 2, Gange));
+        }
+        public static int Beregner(int a, int b, BeregnDelegate funktion)
+        {
+            return funktion(a, b);
+        }
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Divider(int a,int b)
+        {
+            return a / b;
+        }
+        public static int Gange(int a,int b)
+        {
+            return a * b;
+        }
+    }
+}
