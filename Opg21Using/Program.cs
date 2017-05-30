7ce2af3f4454d7e6566aab3db51130338a48dbec
admin <admin@AR-113-07>
2017-05-30 10:51:13 +0200

    Opg21

---
+++ Opg21Using/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Opg21Using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (StreamReader stream = System.IO.File.OpenText("dyrenavne.txt"))
+            {
+                while (stream.Peek() != -1)
+                {
+                    string navn = stream.ReadLine();
+                    Console.WriteLine(navn);
+
+                }
+            }
+        }
+    }
+}
