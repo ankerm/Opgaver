a2e90c5a36979fdfdb7e0ee7a8747a2fd0ff3fe7
admin <admin@AR-113-07>
2017-05-30 15:55:58 +0200

    Opg25

---
+++ Opg25LINQ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg25LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new PersonNuGetPackage.PersonRepositoryRandom();
+            var people 
+        }
+    }
+}
