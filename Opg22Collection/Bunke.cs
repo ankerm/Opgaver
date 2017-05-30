40d288b3b7ee032eabf5a4f7a3cf434aad56880c
admin <admin@AR-113-07>
2017-05-30 11:14:21 +0200

    Opg22

---
+++ Opg22Collection/Bunke.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg22Collection
+{
+    class Bunke
+    {
+        private Stack<Kort> b = new Stack<Kort>();
+
+        public void TilføjKort(Kort k)
+        {
+            b.Push(k);
+        }
+        public Kort FjernKort()
+        {
+            return b.Pop();
+        }
+
+        public void Vis ()
+        {
+            foreach (var k in b)
+            {
+                Console.WriteLine(k.ToString());
+            }
+        }
+    }
+
+}
